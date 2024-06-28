879552634542336cf6a30b960d82f170d50f6823
Alexsandr <ak.polyanskiy@gmail.com>
2024-06-28 11:24:30 +0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -3,12 +3,13 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    # Wait for user input
-    undefined_command(input())
-
+        # Wait for user input
+        undefined_command(input())
+    
 def undefined_command(command):
     print(f"{command}: command not found")
 
