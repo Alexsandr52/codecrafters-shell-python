96b276f1e8b9e6bac90559ec2b129d7720d840c2
Alexsandr <ak.polyanskiy@gmail.com>
2024-06-28 11:36:28 +0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import sys
 
-
 def main():
     # Uncomment this block to pass the first stage
     while True:
@@ -8,12 +7,18 @@ def main():
         sys.stdout.flush()
 
         # Wait for user input
-        user_input = input()
-        if user_input.split()[0] == 'exit' and user_input.split()[1] == '0': break
-        undefined_command(user_input)
+        user_input = input().split()
+        try:
+            if user_input[0] == "exit" and user_input[1] == "0": break
+            elif user_input[0] == "echo": print(" ".join(user_input[1:]))
+            else: undefined_command(user_input[0])
+        except: undefined_command(user_input[0])
     
 def undefined_command(command):
     print(f"{command}: command not found")
+
+def echo(input):
+    print(input)
 
 if __name__ == "__main__":
     main()
